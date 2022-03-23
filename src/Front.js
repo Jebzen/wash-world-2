478700dafea1dbde229eae32a186481d437bcd1a
Jebzen <jeb.bentzen@gmail.com>
2022-03-23 22:55:50 +0100

    Router + navbar

---
+++ src/Front.js
@@ -0,0 +1,7 @@
+export default function Front() {
+    return (
+        <div>
+            <h1>Hello Front!</h1>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./Home";
+import Front from "./Front";
 
 const rootElement = document.getElementById("root");
 render(
     <BrowserRouter>
         <React.StrictMode>
+            <App />
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<Front />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
         </React.StrictMode>
