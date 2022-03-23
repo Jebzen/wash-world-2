010b80c616c984628868ccaf337021541dfd875a
Jebzen <jeb.bentzen@gmail.com>
2022-03-23 22:29:54 +0100

    Startup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import info from "./info";
+import WashLocation from "./components/WashLocation.js";
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    axios.get(info.backendUrl + "/locations").then((result) => {
+      //console.log(result.data.response.locations);
+      setLocations(result.data.response.locations);
+    });
+  }, []);
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      {locations.map((location) => {
+        return <WashLocation key={location.id} location={location} />;
+      })}
     </div>
   );
 }
---
+++ src/components/WashLocation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import info from "../info";
+
+function WashLocation({ location }) {
+  function locationClicked() {
+    console.log(location.name, location.id);
+    console.log(getProductUrl());
+  }
+
+  function getProductUrl() {
+    return info.backendUrl + "/cam/" + location.id;
+  }
+
+  return (
+    <div className="washLocation" onClick={locationClicked}>
+      {location.name} - {location.status}
+    </div>
+  );
+}
+
+export default WashLocation;
